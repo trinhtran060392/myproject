7f3892522530432a57a49ff9ec0644c267c7c47c
trinhtran060392 <trinhtv060392@gmail.com>
2015-11-03 15:24:10 +0700

    init project with services were built by maven and rest was built by play framework

---
+++ services/student/src/main/java/com/trinhtv3/services/student/App.java
@@ -0,0 +1,13 @@
+package com.trinhtv3.services.student;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
